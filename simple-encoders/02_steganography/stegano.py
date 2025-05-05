edd4e55470b746b45b1b349bf8ac66f6e7091fa4
KirillAkishin <akishin.ka@yandex.ru>
2025-05-05 13:11:55 +0300

    upd simple steganograpjy encoding method

--- simple-encoders/02_steganography/stegano.py
+++ simple-encoders/02_steganography/stegano.py
@@ -15,16 +15,16 @@ def generate_random_image(filename:str|None='image.png', width=1_000, height=1_0
         im.close()
     return im
 
-class Stegano:
+class Steg:
     """LSB-method"""
 
     def __init__(self):
         pass
 
-    def fractionalize(self, ):
+    def _fractionalize(self, ):
         pass
 
-    def calc_capacity(self, image_data):
+    def _calc_capacity(self, image_data):
         """Calculate image capacity. Returns the number of bits"""
         image_capacity = np.prod(image_data.shape)
         cap_b = int(image_capacity)
@@ -34,50 +34,90 @@ class Stegano:
         logger.info(f"carrier image capacity: {cap_b} b = {cap_B} B = {cap_KiB} KiB")
         return cap_b
 
-    def insertion(self, image_data, payload_bytes):
-        if args.lsb is not True:
-            return self.insertion_lsb(image_data, payload_bytes)
-        return self.insertion_easy(image_data, payload_bytes)
+    def encode(self, sec:str, cov:str, pld:str='', mode:str='easy') -> bool:
+        def get_sec_bytes(sec):
+            if os.path.isfile(sec):
+                with open(sec, 'rb') as f:
+                    sec_bytes = f.read()
+                    logger.info(f"Encodes the file: '{sec}'")
+            else:
+                sec_bytes = sec.encode()
+                logger.info(f"Encodes the phrase: '{sec}'")
+            logger.debug(f"Length (sec): {len(sec_bytes)}")
+            return sec_bytes
 
-    def insertion_easy(self, image_data, payload_bytes):
-        with open("out.jpg", "wb") as out:
-            with open("a.jpg", "rb") as in_a:
-                cover_image = in_a.read()
-                ln_cover = len(cover_image)
-                print(ln_cover)
-                with open("b.pdf", "rb") as in_b:
-                    hidden_data = in_b.read()
-                    out.write(cover_image)
-                    out.write(hidden_data)
+        def get_pld(cov, pld):
+            if pld == '':
+                basename = os.path.basename(cov)
+                fn, ext = os.path.splitext(basename)
+                dts = str(datetime.now().timestamp()).replace('.','')
+                pld = f"{fn}@{dts}{ext}"
+            return pld
 
-    def insertion_lsb(self, image_data, payload_bytes):
-        payload_header = "{:016}".format(len(payload_bytes)).encode()
-        payload = list(payload_header + payload_bytes)
-        new_image_data = []
-        payload_bits = list()  # a list of bits, '1' and '0' strings
-        for row in image_data:
-            new_row = []
-            for pixel in row:
-                new_pixel = []
-                for item in pixel:
-                    # each item is one color channel: red, green, or blue
-                    if not payload_bits:
-                        if payload:
-                            payload_byte = payload.pop(0)
-                            # convert the byte to a list of '1' and '0' bit strings
-                            payload_bits = list('{:08b}'.format(payload_byte))
-                    if not payload_bits:
-                        new_item = item  # if no more payload, copy item unchanged
-                    else:
-                        # Change the last bit of the item to the next bit of the payload
-                        next_payload_bit = payload_bits.pop(0)
-                        item_bit_string = "{:08b}".format(item)
-                        new_item_string = item_bit_string[:-1] + next_payload_bit
-                        new_item = int(new_item_string, 2)  # convert from binary string to int
-                    new_pixel.append(new_item)
-                new_row.append(new_pixel)
-            new_image_data.append(new_row)
-        return np.array(new_image_data)
+        def check_pld(pld:str, num:int|None=None):
+            with open(pld, 'rb') as pld_file:
+                pld_bytes = pld_file.read()
+                pld_len = len(pld_bytes)
+                logger.debug(f"Length (pld): {pld_len}")
+            if num:
+                return (pld_len == num)
+            return True
+
+        sec_bytes = get_sec_bytes(sec)
+        pld = get_pld(cov, pld)
+        if mode == 'easy':
+            ok = self.insertion_easy(sec_bytes, cov, pld)
+        if mode == 'lsb':
+            ok = self.insertion_lsb(sec_bytes, cov, pld)
+        ok &= check_pld(pld)
+        return ok
+
+    def insertion_easy(self, sec_bytes:str, cov:str, pld:str):
+        with open(cov, "rb") as cov_file:
+            cov_bytes = cov_file.read()
+            logger.debug(f"Length (cov): {len(cov_bytes)}")
+        with open(pld, "wb") as pld_file:
+            pld_file.write(cov_bytes)
+            pld_file.write(sec_bytes)
+        return True
+
+    def insertion_lsb(self, sec_bytes, cov, pld):
+        def lsb_func(cov_data, sec_bytes):
+            payload_header = "{:016}".format(len(sec_bytes)).encode()
+            payload = list(payload_header + sec_bytes)
+            new_image_data = []
+            payload_bits = list()  # a list of bits, '1' and '0' strings
+            for row in cov_data:
+                new_row = []
+                for pixel in row:
+                    new_pixel = []
+                    for item in pixel:
+                        # each item is one color channel: red, green, or blue
+                        if not payload_bits:
+                            if payload:
+                                payload_byte = payload.pop(0)
+                                # convert the byte to a list of '1' and '0' bit strings
+                                payload_bits = list('{:08b}'.format(payload_byte))
+                        if not payload_bits:
+                            new_item = item  # if no more payload, copy item unchanged
+                        else:
+                            # Change the last bit of the item to the next bit of the payload
+                            next_payload_bit = payload_bits.pop(0)
+                            item_bit_string = "{:08b}".format(item)
+                            new_item_string = item_bit_string[:-1] + next_payload_bit
+                            new_item = int(new_item_string, 2)  # convert from binary string to int
+                        new_pixel.append(new_item)
+                    new_row.append(new_pixel)
+                new_image_data.append(new_row)
+            return np.array(new_image_data)
+
+        with Image.open(cov) as cov_img:
+            cov_data = np.asarray(cov_img)
+            # logger.debug(f"Length (cov): {os.stat(cov).st_size}")
+        pld_data = lsb_func(cov_data, sec_bytes)
+        pld_image = Image.fromarray(pld_data.astype(np.uint8))
+        pld_image.save(pld)
+        return True
 
     def extraction(self, image_data):
         if args.lsb is not True:
@@ -114,25 +154,7 @@ class Stegano:
                         return output_bytes
         logger.error("Payload is incomplete")
 
-    def encode(self, secret_obj:str, cover_image:str|None, output_image:str|None=None) -> bool:
-        if cover_image:
-            img = Image.open(cover_image)
-        else:
-            img = generate_random_image(filename=None)
-        image_data = np.asarray(img)
-        img.close()
-        if os.path.isfile(secret_obj):
-            with open(secret_obj, 'rb') as f:
-                payload_bytes = f.read()
-            logger.info(f"Encodes the file: '{secret_obj}'")
-        else:
-            payload_bytes = secret_obj.encode()
-            logger.info(f"Encodes the phrase: '{secret_obj}'")
-        new_image_data = self.insertion(image_data, payload_bytes)
-        new_image = Image.fromarray(new_image_data.astype(np.uint8))
-        output_image = output_image if output_image else f"{datetime.now().timestamp()}.png"
-        new_image.save(output_image)
-        return True
+
 
     def decode(self, encoded_image:str, secret_obj:str|None=None) -> bool:
         with Image.open(encoded_image) as img:
@@ -153,28 +175,34 @@ class Stegano:
 
 
 def main(args):
-    steg = Stegano()
-    if args.e:
-        assert (args.d is False)
-        return steg.encode(args.target, args.img, args.res)
-    if args.d:
-        assert (args.e is False) and (args.img is None)
-        return steg.decode(args.target, args.res)
+    if args.cov == '':
+        cov
+    steg = Steg()
+    if args.mode == 0:
+        return steg.decode(pld=args.pld, sec=args.sec)
+    if args.mode == 1:
+        return steg.encode(sec=args.sec, cov=args.cov, pld=args.pld, mode='easy')
+    if args.mode == 2:
+        return steg.encode(sec=args.sec, cov=args.cov, pld=args.pld, mode='lsb')
     logger.error(f"See help (--help)")
     return False
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(prog='stegano.py')
-    parser.add_argument('target', type=str, help="Either secret or image with payload")
-    parser.add_argument('--img', type=str, help='Cover image')
-    parser.add_argument('--res', type=str, help='Resulting file')
-    parser.add_argument('--lsb', action='store_true', default=False, help='turn on lsb mode')
-    parser.add_argument('-e', action='store_true', default=False, help='turn on encrypting mode')
-    parser.add_argument('-d', action='store_true', default=False, help='turn on decrypting mode')
+    parser.add_argument('--sec', type=str, help='Secret')
+    parser.add_argument('--cov', type=str, help='Cover file')
+    parser.add_argument('--pld', type=str, help='Payload file')
+    parser.add_argument('--mode', type=int, help='Mode switch (0 — decoding; 1 — lsb-encoding; 2 — easy encoding)')
     args = parser.parse_args()
     logger.debug(f"args::{args}")
     ok = main(args)
-    logger.debug(f"end::{__name__}")
+    logger.debug(f"end::{__name__} with code: {ok}")
+    # python stegano.py --mode=0 --pld=../../data/third_image.png --sec=../../data/fourth_image.png
+    # python stegano.py --mode=1 --sec=../../data/first_image.png --cov=../../data/second_image.png --pld=../../data/third_image.png
+    # python stegano.py --mode=2 --sec=../../data/first_image.png --cov=../../data/second_image.png --pld=../../data/third_image.png
+
+
+
 
 
